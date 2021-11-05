7e6999e76e45c8e1b8eb4485e30a304d465bfd89
Phillip Wirth <phillip.wirth@dataport.de>
2021-11-05 14:57:33 +0100

    hackhack

--- fun.sh
+++ fun.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+set -e
+
 id
 
 echo "nice try"
+
+which nvm
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+echo "source $NVM_DIR/nvm.sh --install" >> "$HOME/.bash_profile"
+
+which nvm
+
+nvm --version
