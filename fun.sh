0fbd2fccf4e0f131a4e49bb620a461bb476c9107
Phillip Wirth <phillip.wirth@dataport.de>
2021-11-10 17:42:01 +0100

    fixed blank action

--- fun.sh
+++ fun.sh
@@ -6,10 +6,3 @@ id
 
 echo "nice try"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-echo "source $NVM_DIR/nvm.sh --install" >> "$HOME/.bash_profile"
-
-which nvm
-
-nvm --version
